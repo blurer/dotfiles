f8a2896aff98161c822cedf164eff2d41676aa74
Bryan <40605557+blurer@users.noreply.github.com>
2022-05-05 17:25:50 +0900

    Update first_ubu-root.sh

--- first_ubu-root.sh
+++ first_ubu-root.sh
@@ -4,6 +4,7 @@ useradd bl
 usermod -aG sudo bl
 mkhomedir_helper bl
 mkdir /home/bl/.ssh/
-wget https://raw.githubusercontent.com/blurer/myBS/master/authorized_keys -o /home/bl/.ssh/authorized_keys
+wget https://raw.githubusercontent.com/blurer/myBS/master/authorized_keys 
+mv authorized_keys /home/bl/.ssh/authorized_keys
 chmod 600 /home/bl/.ssh/authorized_keys
 chown -R bl:bl /home/bl/.ssh/
