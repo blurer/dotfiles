7b706089ddac51ad41c00168ad535591173bd861
Bryan <40605557+blurer@users.noreply.github.com>
2022-05-15 08:49:13 +0900

    Update first_ubu-user.sh

--- first_ubu-user.sh
+++ first_ubu-user.sh
@@ -6,19 +6,11 @@ sudo apt install -y \
 	git \
 	curl \
 	htop \
-	ansible \
 	unzip \
 	zsh \
 	python3-pip \
 	python3-setuptools \
-	fail2ban 
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cd $HOME/
-wget https://raw.githubusercontent.com/blurer/myBS/master/zshrc
-mv zshrc .zshrc
-
-sudo systemctl enable fail2ban
-
-source .zshrc
+wget https://raw.githubusercontent.com/blurer/myBS/main/bashrc
+mv bashrc .bashrc
+source .bashrc
